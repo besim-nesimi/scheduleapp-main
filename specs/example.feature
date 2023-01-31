714e620c6811dbd18ddffd9c716f7641508be73b
bestronaut <nesimibesim@outlook.com>
2023-01-31 16:24:26 +0100

    Nice

--- specs/example.feature
+++ specs/example.feature
@@ -3,10 +3,18 @@ Feature: Admin login, navigation and creation
 
 Scenario: Successful login
 Given I am on the login page
-When I see Nodehill Logo
+When I enter valid info
+Then I should see Nodehill Logo in schedule page
 Then Login is successful
 
-Scenario: Successful navigation
-Given I am on the schedule page
+Scenario: Failed login
+Given I am on the login page
+When I enter invalid info
+Then Login is unsuccessful
+
+Scenario: Successful login and navigation
+Given I am on the login page
+When I enter valid info
+Then I should be logged in and see logo in schedule page
 When I click on the admin button
 Then I visit the admin page
--- specs/step_definitions/example.js
+++ specs/step_definitions/example.js
@@ -9,14 +9,47 @@ cy.url().should('equal', 'http://localhost:7655/')
 })
 
 Given('I am on the login page', () => {
-    cy.login('exempel@school.net', 'abc123')
-    cy.wait(5000) 
+    cy.visit('/login.html')
+    cy.wait(2000) 
 })
 
-When('I see Nodehill Logo', () => {
-    cy.get('document.querySelector("body > div > img")').click()
+When('I enter valid info', () => {
+    cy.get('#email').type('exempel@school.net');
+    cy.get('#password').type('abc123');
+    cy.get('[type="submit"]').click()
+    cy.url().should('equal', 'http://localhost:7655/')
+});
+
+When('I enter invalid info', () => {
+    cy.get('#email').type('exempel@skola.net');
+    cy.get('#password').type('123abc');
+    cy.get('[type="submit"]').click()
+});
+
+Then('I should see Nodehill Logo in schedule page', () => {
+    cy.get('img').should('be.visible')
 });
 
 Then('Login is successful', () => {
-    cy.get('#main-image').should('be.visible')
+    cy.get('header > table > thead > tr > :nth-child(6) > div > a').should('be.visible')
+});
+
+Then('Login is unsuccessful', () => {
+    cy.url().should('include', '/login.html')
+});
+
+Given('I should be logged in and see logo in schedule page', () => {
+  cy.url().should('include', 'http://localhost:7655')
+  cy.get('img').should('be.visible')
+});
+
+When('I click on the admin button', () => {
+    // cy.get('[onclick="location.href='/admin'"]').click();
+    //cy.get('button').contains('Admin').click()
+    cy.get('[onclick="location.href=\'/admin\'"]').click();
+    // cy.visit('/admin/')
+});
+
+Then('I visit the admin page', () => {
+  cy.url().should('include', 'http://localhost:7655/admin');
 });
